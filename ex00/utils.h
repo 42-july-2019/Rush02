880b10957342981346d28ad88899dd55d9b6a211
Nathanael Labreuil <alabreui@student.42.fr>
2019-07-20 14:22:41 +0200

    Add buffer size in utils.h

--- ex00/utils.h
+++ ex00/utils.h
@@ -6,17 +6,21 @@
 /*   By: alabreui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/20 11:54:29 by alabreui          #+#    #+#             */
-/*   Updated: 2019/07/20 12:03:19 by alabreui         ###   ########.fr       */
+/*   Updated: 2019/07/20 14:05:12 by alabreui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_H
 # define UTILS_H
 
+# define BUF_SIZE 36
+
 typedef struct	s_dict_entry
 {
 	int		nb;
 	char	*str;
 }				t_dict_entry;
+
+t_dict_entry	*read_dict(char *dict_name);
 
 #endif
