785c62bf0856092136a133049a17dc2fa9291763
Sofiane Bouattour <sbouatto@student.42.fr>
2019-07-21 16:29:06 +0200

    removed ft_putchar and replaced it with write

--- ex00/main.c
+++ ex00/main.c
@@ -6,40 +6,47 @@
 /*   By: alabreui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/20 12:27:55 by alabreui          #+#    #+#             */
-/*   Updated: 2019/07/21 14:33:23 by alabreui         ###   ########.fr       */
+/*   Updated: 2019/07/21 16:28:33 by sbouatto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include <stdio.h>
 #include <unistd.h>
 #include "utils.h"
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
 void	ft_error(void)
 {
-	ft_putchar('E');
-	ft_putchar('R');
-	ft_putchar('R');
-	ft_putchar('O');
-	ft_putchar('R');
-	ft_putchar('\n');
+	write(1, "ERROR\n", 6);
+}
+
+void	ft_error_dict(void)
+{
+	write(1, "DICT ERROR\n", 11);
 }
 
 int		main(int argc, char **argv)
 {
-	char	*dict_name;
+	char			*dict_name;
+	char			*given_number;
+	t_dict_entry	*array;
 
 	dict_name = "numbers.dict";
-	if (argc == 1)
-		ft_error();
-	if (argc == 2)
+	if (argc < 2 || argc > 3)
 	{
-		if (argv[2] < 0)
-			ft_error();
-	}
-	if (argc > 3)
 		ft_error();
+		return(0);
+	}
+	else if (argc == 2)
+	{
+		given_number = argv[1];
+		while (*given_number)
+		{
+			if (*given_number < '0' || *given_number > '9')
+			{
+				ft_error();
+				return (0);
+			}
+			given_number++;
+		}
+	}
+	return (0);
 }
