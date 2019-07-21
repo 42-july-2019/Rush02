dfff96d7ba209bfa5890ea0fb4dd104f22f38a90
Nathanael Labreuil <alabreui@student.42.fr>
2019-07-21 14:05:00 +0200

    Finish read_dict implementation

--- ex00/read_dict.c
+++ ex00/read_dict.c
@@ -6,7 +6,7 @@
 /*   By: alabreui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/20 12:13:51 by alabreui          #+#    #+#             */
-/*   Updated: 2019/07/20 17:00:59 by alabreui         ###   ########.fr       */
+/*   Updated: 2019/07/21 14:00:58 by alabreui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -25,7 +25,6 @@ int				open_dict_data(char **dict_data, char *dict_name, int *dict_ref)
 	*dict_ref = open(dict_name, O_RDONLY);
 	if (*dict_ref == -1)
 		return (0);
-	printf("%d\n", *dict_ref);
 	nb_times_read = 0;
 	while ((size_read = read(*dict_ref, read_data, BUF_SIZE)))
 		nb_times_read++;
@@ -36,7 +35,6 @@ int				open_dict_data(char **dict_data, char *dict_name, int *dict_ref)
 	*dict_ref = open(dict_name, O_RDONLY);
 	if (*dict_ref == -1)
 		return (0);
-	printf("%d\n", *dict_ref);
 	return (1);
 }
 
@@ -62,25 +60,85 @@ void			get_dict_data(char *dict_full_data, int dict_ref)
 	dict_full_data[total_size_read] = '\0';
 }
 
+int			set_dict_entry_el(t_dict_entry *element, char **strs)
+{
+	int		start;
+	int		length;
+	int		j;
+	char	*curr_str;
+
+	element->nb = ft_atoi(strs[0]);
+	start = 0;
+	while (strs[1][start] == ' ' || strs[1][start] == '\t')
+		start++;
+	length = 0;
+	while (strs[1][start + length])
+		length++;
+	if (!(curr_str =
+				((char *)malloc(sizeof(char) * length + 1))))
+		return (0);
+	j = 0;
+	while (j < length)
+	{
+		curr_str[j] = strs[1][start + j];
+		j++;
+	}
+	curr_str[j] = '\0';
+	element->str = curr_str;
+	return (1);
+}
+
+int			format_dict_data(t_dict_entry **array, char **dict_array)
+{
+	int				i;
+	int				length;
+	char			**curr_strs;
+
+	length = 0;
+	while (dict_array[length] != 0)
+		length++;
+	if (!(*array =
+				((t_dict_entry *)malloc(sizeof(t_dict_entry) * length + 1))))
+		return (0);
+	i = 0;
+	while (i < length)
+	{
+		curr_strs = ft_split(dict_array[i], ':');
+		if (!set_dict_entry_el(&(*array)[i], curr_strs))
+			return (0);
+		curr_strs = 0;
+		i++;
+	}
+	array[i]->str = "";
+	return (1);
+}
+
 t_dict_entry	*read_dict(char *dict_name)
 {
 	int				dict_ref;
 	char			*dict_full_data;
 	char			**dict_array_data;
 	t_dict_entry	*array;
+	int	i;
 
-	array = 0;
 	open_dict_data(&dict_full_data, dict_name, &dict_ref);
 	if (dict_ref == -1)
 		return (0);
 	get_dict_data(dict_full_data, dict_ref);
-	printf("%s\n", dict_full_data);
-	dict_array_data = ft_split(dict_full_data, '\n');
-	while (*dict_array_data)
+	if(!(dict_array_data = ft_split(dict_full_data, '\n')))
+		return (0);
+	if (!format_dict_data(&array, dict_array_data))
+		return (0);
+	i = 0;
+	while (array[i].str)
 	{
-		printf("%s, ", *dict_array_data);
-		dict_array_data++;
+		printf("%d : %s, ", array[i].nb, array[i].str);
+		i++;
 	}
+	free(dict_full_data);
+	dict_full_data = 0;
+	free(dict_array_data);
+	dict_array_data = 0;
 	if (!close(dict_ref))
 		return (0);
 	return (array);
